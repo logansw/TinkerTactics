0c8f1c489276b176144f0f2b1cba4a811a57395d
logansw <logansw428@gmail.com>
2024-07-16 14:41:47 -0700

    refactor: Remove unused methods from HUDManager

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -17,22 +17,4 @@ public class HUDManager : Singleton<HUDManager>
     {
         tower.TargetTracker.DisplayRange(false);
     }
-
-    public void DisplayAppliedUpgrades(Tower tower)
-    {
-        for (int i = 0; i < tower.SelectedUpgrades.Count; i++)
-        {
-            Debug.Log($"{tower.SelectedUpgrades[i].Name}: {tower.SelectedUpgrades[i].Description}");
-        }
-    }
-
-    public void DisplayAvailableUpgrades(Tower tower)
-    {
-        List<Upgrade> availableUpgrades = tower.Upgrades[tower.Tier];
-        for (int i = 0; i < availableUpgrades.Count; i++)
-        {
-            Upgrade upgrade = availableUpgrades[i];
-            Debug.Log($"{upgrade.Name} ({upgrade.Cost}): {upgrade.Description}");
-        }
-    }
 }
