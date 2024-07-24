c6dfbaaae92b1fd1a20c2c329a1a32bd87f3f47d
logansw <logansw428@gmail.com>
2024-07-24 11:52:48 -0700

    UI: Healthbar displays health in numbers

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
@@ -8,11 +10,13 @@ public class Healthbar : MonoBehaviour
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private Transform _remainingHealth;
     [SerializeField] private GameObject _breakpointPrefab;
+    [SerializeField] private TMP_Text _healthText;
 
     public void Initialize(Health health)
     {
         RegisterHealth(health);
         DrawBreakpoints();
+        UpdateHealthbar(health.CurrentHealth, health.MaxHealth);
     }
 
     public void RegisterHealth(Health health)
@@ -25,6 +29,7 @@ public class Healthbar : MonoBehaviour
     {
         float healthPercentage = currentHealth / maxHealth;
         _remainingHealth.localScale = new Vector3(healthPercentage, 1, 1);
+        _healthText.text = $"{currentHealth}/{maxHealth}";
     }
 
     private void DrawBreakpoints()
