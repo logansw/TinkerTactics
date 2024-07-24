1ab793234016cfd28c177502f83ad4850e3563bd
logansw <logansw428@gmail.com>
2024-07-23 17:01:22 -0700

    fix: Cleaned up item UI to match currency change

--- Assets/Scripts/Shop/ItemUI.cs
+++ Assets/Scripts/Shop/ItemUI.cs
@@ -8,7 +8,6 @@ public class ItemUI : MonoBehaviour
     public TMP_Text Name;
     public TMP_Text Description;
     public TMP_Text YellowCost;
-    public GameObject Border;
     private TowerItemSO item;
 
     public void PopulateFields(TowerItemSO item)
